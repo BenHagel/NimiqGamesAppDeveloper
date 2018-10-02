b8981a34637b9b5475ee8f8842fdb317f44c101d
Benjamin Hagel <bhag@Benjamins-MacBook-Pro.local>
2018-10-02 05:07:03 -0400

    added documentation part 1

--- public/Navigator.js
+++ public/Navigator.js
@@ -1,1 +1,1 @@
-function Navigator_arrival() {Menu.isMobileUser=false;Menu.signature='nq7469349239677956u1538462635239';document.getElementById('activeUsers').innerText = '  Active users: ' + 1;ServerAPI.updateBalanceAndWallet();Menu.landingScreen='normal';Menu.initer();}
+function Navigator_arrival() {Menu.isMobileUser=false;Menu.signature='nq2355777337140088u1538471185765';document.getElementById('activeUsers').innerText = '  Active users: ' + 2;ServerAPI.updateBalanceAndWallet();Menu.landingScreen='normal';Menu.initer();}
